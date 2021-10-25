196d9c4d74034686bdc54b8e05319406e5cfc3f7
Arkady Seleznev <a.seleznev@me.com>
2021-10-25 17:42:01 +0700

    feat: added possibility to log/not log joke

--- src/joke.ts
+++ src/joke.ts
@@ -22,18 +22,21 @@ type data = {
 
 /**
  * Output a joke of a given type to the consol (using .then/.catch)
- * @param jokeType - Joke category/type ('any', 'programming', 'misc', 'dark', 'pun', 'spooky', 'christmas')
- * @returns Nothing
+ * @param {string} jokeType - Joke category/type ('any', 'programming', 'misc', 'dark', 'pun', 'spooky', 'christmas')
+ * @param {boolean=true} logJoke - To console.log received joke or not
+ * @returns {void} Nothing
  */
-export function getJokeWithThen(jokeType: string): void {
+export function getJokeWithThen(jokeType: string, logJoke: boolean = true): void {
     axios.get<data>('https://v2.jokeapi.dev/joke/' + jokeType)
         .then((response) => response.data)
         .then((jokeData) => {
-            if (jokeData.type === 'single') {
-                console.log('\n' + jokeData.joke + '\n')
-            } else {
-                console.log('\n' + jokeData.setup + '\n')
-                setTimeout(() => console.log(jokeData.delivery + '\n'), 2000)
+            if (logJoke) {
+                if (jokeData.type === 'single') {
+                    console.log('\n' + jokeData.joke + '\n')
+                } else {
+                    console.log('\n' + jokeData.setup + '\n')
+                    setTimeout(() => console.log(jokeData.delivery + '\n'), 2000)
+                }
             }
         })
         .catch(err => console.log(err))
@@ -41,19 +44,22 @@ export function getJokeWithThen(jokeType: string): void {
 
 /**
  * Output a joke of a given type to the consol (using async/await)
- * @param jokeType - Joke category/type ('any', 'programming', 'misc', 'dark', 'pun', 'spooky', 'christmas')
- * @returns Promise
+ * @param {string} jokeType - Joke category/type ('any', 'programming', 'misc', 'dark', 'pun', 'spooky', 'christmas')
+ * @param {boolean=true} logJoke To console.log received joke or not
+ * @returns {Promise<void>} Promise
  */
-export async function getJokeWithAsync(jokeType: string): Promise<void> {
+export async function getJokeWithAsync(jokeType: string, logJoke: boolean = true): Promise<void> {
 
     try {
         let response = await axios.get<data>('https://v2.jokeapi.dev/joke/' + jokeType)
-        let jokeData = response.data
-        if (jokeData.type === 'single') {
-            console.log('\n' + jokeData.joke + '\n')
-        } else {
-            console.log('\n' + jokeData.setup + '\n')
-            setTimeout(() => console.log(jokeData.delivery + '\n'), 2000)
+        if (logJoke) {
+            let jokeData = response.data
+            if (jokeData.type === 'single') {
+                console.log('\n' + jokeData.joke + '\n')
+            } else {
+                console.log('\n' + jokeData.setup + '\n')
+                setTimeout(() => console.log(jokeData.delivery + '\n'), 2000)
+            }
         }
     } catch(err) {
         console.log(err)
